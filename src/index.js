9b504dadada06de753121e02131a4d1e5690030c
omsemwal <omsemwal111@gmail.com>
2022-08-26 17:24:07 +0530

    authentication

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Hariom:CN2CFpJNl2lt1Sqy@cluster0.ngsk6.mongodb.net/semwal-dbtest", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
